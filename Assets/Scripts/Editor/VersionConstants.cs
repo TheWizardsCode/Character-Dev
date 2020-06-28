2e382a9348356786cb607f231736d19849ee0b37
Ross Gardler <ross.gardler@microsoft.com>
2020-06-28 11:47:52 -0800

    Add Package managifest and version constants

---
+++ Assets/Scripts/Editor/VersionConstants.cs
@@ -0,0 +1,33 @@
+namespace org.WizardsCode.Character.Editor
+{
+	/// <summary>
+	/// Version info for this library.
+	/// </summary>
+	internal static class VersionConstants
+	{
+		/// <summary>
+		/// The semantic version
+		/// </summary>
+		public const string VERSION = "0.0.1";
+
+		/// <summary>
+		/// The branch of GIT this package was published from.
+		/// </summary>
+		public const string GIT_BRANCH = "master";
+
+		/// <summary>
+		/// The current GIT commit hash this package was published on.
+		/// </summary>
+		public const string GIT_COMMIT = "64ab7d3798534dbc988f87df09021cda21b2e390";
+
+		/// <summary>
+		/// The UTC human-readable date this package was published at.
+		/// </summary>
+		public const string PUBLISH_DATE = "Sunday, June 28, 2020";
+
+		/// <summary>
+		/// The UTC time this package was published at.
+		/// </summary>
+		public const string PUBLISH_TIME = "06/28/2020 19:47:19";
+	}
+}
