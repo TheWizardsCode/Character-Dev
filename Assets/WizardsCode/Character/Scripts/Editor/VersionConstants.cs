4679dcf29333159e30374cb6ea27b40c985cc4d4
Ross Gardler <ross@gardler.org>
2021-01-28 20:40:39 -0800

    Add CiDy pedestrian system

--- Assets/WizardsCode/Character/Scripts/Editor/VersionConstants.cs
+++ Assets/WizardsCode/Character/Scripts/Editor/VersionConstants.cs
@@ -8,26 +8,26 @@ namespace WizardsCode.Character
 		/// <summary>
 		/// The semantic version
 		/// </summary>
-		public const string VERSION = "0.0.6";
+		public const string VERSION = "0.0.8";
 
 		/// <summary>
 		/// The branch of GIT this package was published from.
 		/// </summary>
-		public const string GIT_BRANCH = "master";
+		public const string GIT_BRANCH = "devtest";
 
 		/// <summary>
 		/// The current GIT commit hash this package was published on.
 		/// </summary>
-		public const string GIT_COMMIT = "cdd95ecd78da13dcc73575710a78e9a5724e0c9e";
+		public const string GIT_COMMIT = "4436a76c973154b50bf0a3161e02fec50e9d1809";
 
 		/// <summary>
 		/// The UTC human-readable date this package was published at.
 		/// </summary>
-		public const string PUBLISH_DATE = "Monday, January 18, 2021";
+		public const string PUBLISH_DATE = "Friday, January 29, 2021";
 
 		/// <summary>
 		/// The UTC time this package was published at.
 		/// </summary>
-		public const string PUBLISH_TIME = "01/18/2021 19:40:46";
+		public const string PUBLISH_TIME = "01/29/2021 04:40:13";
 	}
 }
