5b73b221e47525d817e40c82d7ae23c7060d5065
Ross Gardler <ross@gardler.org>
2021-02-11 14:27:50 -0800

    Update version numbers for release

--- Assets/WizardsCode/Character/Scripts/Editor/VersionConstants.cs
+++ Assets/WizardsCode/Character/Scripts/Editor/VersionConstants.cs
@@ -8,7 +8,7 @@ namespace WizardsCode.Character
 		/// <summary>
 		/// The semantic version
 		/// </summary>
-		public const string VERSION = "0.0.9";
+		public const string VERSION = "0.0.11";
 
 		/// <summary>
 		/// The branch of GIT this package was published from.
@@ -18,16 +18,16 @@ namespace WizardsCode.Character
 		/// <summary>
 		/// The current GIT commit hash this package was published on.
 		/// </summary>
-		public const string GIT_COMMIT = "3b58f9c6b22ecd40722979ef61f5d9c5a5fb655a";
+		public const string GIT_COMMIT = "2e47b34a6d318bd26182b3032205555026f54d21";
 
 		/// <summary>
 		/// The UTC human-readable date this package was published at.
 		/// </summary>
-		public const string PUBLISH_DATE = "Friday, February 5, 2021";
+		public const string PUBLISH_DATE = "Thursday, February 11, 2021";
 
 		/// <summary>
 		/// The UTC time this package was published at.
 		/// </summary>
-		public const string PUBLISH_TIME = "02/05/2021 04:18:13";
+		public const string PUBLISH_TIME = "02/11/2021 22:27:13";
 	}
 }
