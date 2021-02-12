9306f508c1555a939fbb91cf45abca9c48d0b25c
Ross Gardler <ross@gardler.org>
2021-02-11 16:00:49 -0800

    avoid null reference in custom editor

--- Assets/WizardsCode/Character/Documentation/Behaviours/Mini God Game - Step 001.md
+++ Assets/WizardsCode/Character/Documentation/Behaviours/Mini God Game - Step 001.md
@@ -5,3 +5,5 @@ You can, however, influence the world they live in.
 You start out with 2 people in your population. Fortunately, one is male and the other is female. Your first job is to encourage them to build a house. to build a house they will need wood. So plant a few trees.
 
 Simply point and click to plant trees.
+
+The male (red) character will go an chop the trees down. But this takes up alowt of energy. When he's tired he will lie down to rest.
--- Assets/WizardsCode/Character/Scripts/Editor/GenericAIBehaviourEditor.cs
+++ Assets/WizardsCode/Character/Scripts/Editor/GenericAIBehaviourEditor.cs
@@ -33,48 +33,49 @@ namespace WizardsCode.Character
             if (stats != null)
             {
                 EditorGUILayout.LabelField("Required Stats (" + stats.Length + ")", EditorStyles.boldLabel);
-            } else
+            
+                for (int i = 0; i < stats.Length; i++)
+                {
+                    if (i >= showRequiredStat.Count) showRequiredStat.Add(false);
+
+                    string label = stats[i].statTemplate.DisplayName;
+                    switch (stats[i].objective)
+                    {
+                        case Objective.GreaterThan:
+                            label += " > ";
+                            break;
+                        case Objective.Approximately:
+                            label += " ~= ";
+                            break;
+                        case Objective.LessThan:
+                            label += " < ";
+                            break;
+                    }
+                    label += stats[i].Value;
+                    label += " (" + stats[i].NormalizedValue + ")";
+
+                    showRequiredStat[i] = EditorGUILayout.BeginFoldoutHeaderGroup(showRequiredStat[i], label);
+
+                    if (showRequiredStat[i])
+                    {
+                        GUILayout.BeginHorizontal();
+                        stats[i].objective = (Objective)EditorGUILayout.EnumPopup("Objective", stats[i].objective);
+                        GUILayout.EndHorizontal();
+
+                        GUILayout.BeginHorizontal();
+                        EditorGUILayout.PrefixLabel("Value");
+                        stats[i].Value = EditorGUILayout.FloatField(stats[i].Value);
+                        EditorGUILayout.PrefixLabel("Normalized");
+                        stats[i].NormalizedValue = EditorGUILayout.FloatField(stats[i].NormalizedValue);
+                        GUILayout.EndHorizontal();
+                    }
+
+                    EditorGUILayout.EndFoldoutHeaderGroup();
+                }
+            }
+            else
             {
                 EditorGUILayout.LabelField("No Required Stats");
-            }
-            
-            for (int i = 0; i < stats.Length; i++)
-            {
-                if (i >= showRequiredStat.Count) showRequiredStat.Add(false);
-
-                string label = stats[i].statTemplate.DisplayName;
-                switch (stats[i].objective)
-                {
-                    case Objective.GreaterThan:
-                        label += " > ";
-                        break;
-                    case Objective.Approximately:
-                        label += " ~= ";
-                        break;
-                    case Objective.LessThan:
-                        label += " < ";
-                        break;
-                }
-                label += stats[i].Value;
-                label += " (" + stats[i].NormalizedValue + ")";
-
-                showRequiredStat[i] = EditorGUILayout.BeginFoldoutHeaderGroup(showRequiredStat[i], label);
-
-                if (showRequiredStat[i])
-                {
-                    GUILayout.BeginHorizontal();
-                    stats[i].objective = (Objective)EditorGUILayout.EnumPopup("Objective", stats[i].objective);
-                    GUILayout.EndHorizontal();
-
-                    GUILayout.BeginHorizontal();
-                    EditorGUILayout.PrefixLabel("Value");
-                    stats[i].Value = EditorGUILayout.FloatField(stats[i].Value);
-                    EditorGUILayout.PrefixLabel("Normalized");
-                    stats[i].NormalizedValue = EditorGUILayout.FloatField(stats[i].NormalizedValue);
-                    GUILayout.EndHorizontal();
-                }
-
-                EditorGUILayout.EndFoldoutHeaderGroup();
             }
 
             EditorGUILayout.EndVertical();
