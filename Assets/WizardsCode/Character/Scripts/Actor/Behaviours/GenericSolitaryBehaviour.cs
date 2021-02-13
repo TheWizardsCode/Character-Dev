643bed1d8ef83637433132240440af3815ee3fb0
Ross Gardler <ross@gardler.org>
2021-02-13 12:47:39 -0800

    Add a generic solitary behaviour

---
+++ Assets/WizardsCode/Character/Scripts/Actor/Behaviours/GenericSolitaryBehaviour.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WizardsCode.Character
+{
+    /// <summary>
+    /// A Generic Solitary Behaviour is one that does not need another actor
+    /// or interactable to be performed. It also does not produce anything
+    /// outside of changes to the actors stats.
+    /// </summary>
+    public class GenericSolitaryBehaviour : AbstractAIBehaviour
+    {
+
+    }
+}
