04fd260353d7862462a6515aba85e4fbc01f89e2
Ross Gardler <ross@gardler.org>
2021-03-07 11:21:38 -0800

    Add a crouch button to the player controller

--- Assets/WizardsCode/Character/Scripts/Utility/ClickToMove.cs
+++ Assets/WizardsCode/Character/Scripts/Utility/ClickToMove.cs
@@ -10,15 +10,23 @@ namespace WizardsCode.Utility
     /// </summary>
     public class ClickToMove : MonoBehaviour
     {
+        [SerializeField, Tooltip("The parameter name to make the animator enter crouch mode.")]
+        string m_CrouchParamName = "Crouch";
+
         NavMeshAgent m_Agent;
+        Animator m_Animator;
+        int m_CrouchHash;
 
         void Awake()
         {
             m_Agent = GetComponent<NavMeshAgent>();
+            m_Animator = GetComponent<Animator>();
+            m_CrouchHash = Animator.StringToHash(m_CrouchParamName);
         }
 
         private void Update()
         {
+            // LMB
             if (Input.GetMouseButtonDown(0))
             {
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -27,6 +35,12 @@ namespace WizardsCode.Utility
                 {
                     m_Agent.SetDestination(hitInfo.point);
                 }
+            }
+
+            // Crouch
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                m_Animator.SetBool(m_CrouchHash, !m_Animator.GetBool(m_CrouchHash));
             }
         }
     }
