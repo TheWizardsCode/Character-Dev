52616e386ab3d410287f750bd541dbc1f040597e
Nathan Schuetz <mail@nathanschuetz.ca>
2021-03-13 11:57:33 -0700

    bus scene v1

---
+++ Assets/_nathan/scripts/MusicTracks.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace _nathan.scripts
+{
+[CreateAssetMenu(fileName = "NewMusicTrackList", menuName = "inkjam_SOs")]
+public class MusicTracks : ScriptableObject
+{
+    [SerializeField] public List<AudioSource_NameSourcePair> AudioClips;
+}
+}
