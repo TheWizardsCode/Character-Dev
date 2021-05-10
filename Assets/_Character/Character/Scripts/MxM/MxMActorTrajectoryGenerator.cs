f1f232cea6c58fe826faa4eaec33482417161cfc
Ross Gardler <ross@gardler.org>
2021-05-10 00:56:30 -0700

    When reaching the player enter the unarmed combat state. Create unarmed combat state - idle only right now

---
+++ Assets/_Character/Character/Scripts/MxM/MxMActorTrajectoryGenerator.cs
@@ -0,0 +1,33 @@
+﻿#if MXM_PRESENT
+using MxM;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WizardsCode.Character
+{
+    public class MxMActorTrajectoryGenerator : MxMTrajectoryGenerator_BasicAI
+    {
+        public Vector3 desiredVelocity
+        {
+            get { return m_navAgent.desiredVelocity; }
+        }
+
+        public float remainingDistance
+        {
+            get { return m_navAgent.remainingDistance; }
+        }
+
+        public void SetTrajectoryConfig(MxMActorTrajectoryGeneratorConfig config)
+        {
+            if (config == null)
+                return;
+
+            MaxSpeed = config.MaxSpeed;
+            MoveResponsiveness = config.MoveResponsiveness;
+            TurnResponsiveness = config.TurnResponsiveness;
+            TrajectoryMode = config.TrajectoryMode;
+        }
+    }
+}
+#endif
