892e317c3d42d32dab26be1cb35e62b7ef666744
Ross Gardler <ross@gardler.org>
2021-05-09 22:54:08 -0700

    Flee from Player

--- Assets/_Character/Character/Scripts/Stats/AbstractSense.cs
+++ Assets/_Character/Character/Scripts/Stats/AbstractSense.cs
@@ -22,9 +22,21 @@ namespace WizardsCode.Character.AI
         LayerMask m_LayerMask = 1;
         [SerializeField, Tooltip("The maximum number of sensed objects.")]
         int maxSensedColliders = 50;
+        [SerializeField, Tooltip("The name of the Component type we require the sensed object to have.")]
+        //TODO don't require fully qualified name here
+        string m_ComponentTypeNameToSense = "WizardsCode.Stats.StatsTracker";
+        [SerializeField, Tooltip("An (optional) influencer to apply to the actor if they sense anything.")]
+        StatInfluencerSO m_StatInfluencer;
 
         internal string logName;
+        private Type m_ComponentType;
         private List<Transform> m_SensedObjects = new List<Transform>();
+        bool isValid = true;
+
+        internal Type ComponentType
+        {
+            get { return m_ComponentType; }
+        }
 
         internal List<Transform> SensedThings
         {
@@ -42,13 +54,19 @@ namespace WizardsCode.Character.AI
         internal virtual void Awake()
         {
             logName = transform.root.name;
+
+            m_ComponentType = Type.GetType(m_ComponentTypeNameToSense);
+            if (m_ComponentType == null)
+            {
+                Debug.LogWarning(logName + " is a sense that has an invalid target component type of " + m_ComponentType + " Disabling the sense component.");
+                this.enabled = false;
+            }
         }
 
         internal void Update()
         {
             //TODO move the overall sense code into the ActorController where it can cache the sensed object list. Implementations of this class can then filter for items they care about.
             Collider[] hitColliders = new Collider[maxSensedColliders];
-            //TODO provide a layermask for the senses
             int numColliders = Physics.OverlapSphereNonAlloc(transform.position, range, hitColliders, m_LayerMask);
             m_SensedObjects = new List<Transform>();
             for (int i = numColliders - 1; i >= 0; i--)
@@ -58,13 +76,34 @@ namespace WizardsCode.Character.AI
                 {
                     continue;
                 }
-                m_SensedObjects.Add(root);
+
+                if (root.GetComponentInChildren(ComponentType))
+                {
+                    m_SensedObjects.Add(root);
+                }
             }
 
             OnUpdate();
         }
 
         internal virtual void OnUpdate() { }
+
+        private void OnValidate()
+        {
+            Type componentType = Type.GetType(m_ComponentTypeNameToSense);
+            if (componentType == null && isValid)
+            {
+                isValid = false;
+                //TODO handle this error in the editor, e.g. show an error box
+                Debug.LogWarning(name + " has a sense SO attempting to sense an unkown object type of " + componentType);
+            }
+            else if (!isValid)
+            {
+                isValid = true;
+                //TODO when the error is reported in the editor no need for this log
+                Debug.Log(name + " is now valid.");
+            }
+        }
 
     }
 }
--- Assets/_Character/Character/Scripts/Stats/Senses/SightSense.cs
+++ Assets/_Character/Character/Scripts/Stats/Senses/SightSense.cs
@@ -8,68 +8,22 @@ namespace WizardsCode.Character.AI
     /// </summary>
     public class SightSense : AbstractSense
     {
-        [SerializeField, Tooltip("The name of the Component type we require the sensed object to have.")]        
-        //TODO don't require fully qualified name here
-        string m_ComponentTypeNameToSense = "WizardsCode.Character.ActorController";
-
-        bool isValid = true;
-        private Type m_ComponentType;
         Transform m_closestSensedObject;
-
-        internal Type ComponentType
-        {
-            get { return m_ComponentType; }
-        }
-
-        internal override bool HasSensed
-        {
-            get { return m_closestSensedObject != null; }
-        }
-
-        internal override void Awake()
-        {
-            base.Awake();
-            m_ComponentType = Type.GetType(m_ComponentTypeNameToSense);
-            if (m_ComponentType == null)
-            {
-                Debug.LogWarning(logName + " is a sense that has an invalid target component type of " + m_ComponentType + " Disabling the sense component.");
-                this.enabled = false;
-            }
-        }
 
         internal override void OnUpdate()
         {
+            //TODO OPTIMIZATION don't just find the closest, check the actor can actually see the actor (should probably be done in a `bool isSensed()` that checks they are seen when first detected rather than iterate over them a second time here.
             m_closestSensedObject = null;
             float minSqrMag = float.PositiveInfinity;
             float currentSqrMag;
             for (int i = SensedThings.Count - 1; i >= 0; i--)
             {
-                if (SensedThings[i].GetComponent(ComponentType))
+                currentSqrMag = Vector3.SqrMagnitude(SensedThings[i].position - transform.position);
+                if (currentSqrMag < minSqrMag)
                 {
-                    currentSqrMag = Vector3.SqrMagnitude(SensedThings[i].position - transform.position);
-                    if (currentSqrMag < minSqrMag)
-                    {
-                        minSqrMag = currentSqrMag;
-                        m_closestSensedObject = SensedThings[i];
-                    }
+                    minSqrMag = currentSqrMag;
+                    m_closestSensedObject = SensedThings[i];
                 }
-            }
-        }
-
-        private void OnValidate()
-        {
-            Type componentType = Type.GetType(m_ComponentTypeNameToSense);
-            if (componentType == null && isValid)
-            {
-                isValid = false;
-                //TODO handle this error in the editor, e.g. show an error box
-                Debug.LogWarning(name + " has a sense SO attempting to sense an unkown object type of " + componentType);
-            }
-            else if (!isValid)
-            {
-                isValid = true;
-                //TODO when the error is reported in the editor no need for this log
-                Debug.Log(name + " is now valid.");
             }
         }
     }
